a76ba29ddbf12446ba061afc0d4c48d7533fbad6
BrianF2023 <125948192+BrianF2023@users.noreply.github.com>
2023-03-30 01:23:15 -0400

    In Progress

---
+++ Memory Game/Assets/MemoryCard.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] GameObject cardBack;
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf) { cardBack.SetActive(false); }
+    }
+}
