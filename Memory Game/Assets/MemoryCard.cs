6c50154f6bbd0be2b24b55f15d0094044f85a5d2
BrianF2023 <125948192+BrianF2023@users.noreply.github.com>
2023-04-04 01:23:24 -0400

    Memory Card Update

--- Memory Game/Assets/MemoryCard.cs
+++ Memory Game/Assets/MemoryCard.cs
@@ -7,6 +7,27 @@ public class MemoryCard : MonoBehaviour
     [SerializeField] GameObject cardBack;
     public void OnMouseDown()
     {
-        if (cardBack.activeSelf) { cardBack.SetActive(false); }
+        if (cardBack.activeSelf && controller.canReveal) 
+        { 
+            cardBack.SetActive(false);
+            controller.CardRevealed(this);
+        }
     }
-}
+    [SerializeField] SceneController controller;
+    private int _id;
+    public int Id
+    {
+        get { return _id; }
+    }
+    public void Unreveal()
+    {
+        cardBack.SetActive(true);
+    }
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+        
+ }
+
