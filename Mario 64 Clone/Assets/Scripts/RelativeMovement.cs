0d963911c8600da652b39f4b6f91e60e5d60b920
BrianF2023 <125948192+BrianF2023@users.noreply.github.com>
2023-05-15 00:22:54 -0400

    Updated

--- Mario 64 Clone/Assets/Scripts/RelativeMovement.cs
+++ Mario 64 Clone/Assets/Scripts/RelativeMovement.cs
@@ -9,11 +9,19 @@ public class RelativeMovement : MonoBehaviour
 
     public float rotSpeed = 15.0f;
     public float moveSpeed = 6.0f;
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10.0f;
+    public float minFall = -1.5f;
+
+    private float vertSpeed;
 
     private CharacterController charController;
+    private ControllerColliderHit contact;
 
     void Start()
     {
+        vertSpeed = minFall;
         charController = GetComponent<CharacterController>();
     }
 
@@ -35,7 +43,70 @@ public class RelativeMovement : MonoBehaviour
             Quaternion direction = Quaternion.LookRotation(movement);
             transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
         }
+
+        bool hitGround = false;
+        RaycastHit hit;
+        if (vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            float check = (charController.height + charController.radius) / 1.9f;
+            hitGround = hit.distance <= check;
+        }
+        if (hitGround)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                vertSpeed = jumpSpeed;
+            }
+            else
+            {
+                vertSpeed = minFall;
+            }
+        }
+        else
+        {
+            vertSpeed += gravity * 5 * Time.deltaTime;
+            if (vertSpeed < terminalVelocity)
+            {
+                vertSpeed = terminalVelocity;
+            }
+            if (charController.isGrounded)
+            {
+                if (Vector3.Dot(movement, contact.normal) < 0)
+                {
+                    movement = contact.normal * moveSpeed;
+                }
+                else
+                {
+                    movement += contact.normal * moveSpeed;
+                }
+            }
+        }
+
+        if (charController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                vertSpeed = jumpSpeed;
+            }
+            else
+            {
+                vertSpeed = minFall;
+            }
+        }
+        else
+        {
+            vertSpeed += gravity * 5 * Time.deltaTime;
+            if (vertSpeed < terminalVelocity)
+            {
+                vertSpeed = terminalVelocity;
+            }
+        }
+        movement.y = vertSpeed;
         movement *= Time.deltaTime;
         charController.Move(movement);
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        contact = hit;
     }
 }
